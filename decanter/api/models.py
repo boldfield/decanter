1396484cbc9cb78c05721d034a454cf3b71ccc88
Brian Oldfield <brian.oldfield@gmail.com>
2012-08-02 23:48:07 -0700

    [#33485909] Initial commit of api models, included tables for simple rating and tagging system.

--- decanter/api/models.py
+++ decanter/api/models.py
@@ -12,9 +12,10 @@ db = SQLAlchemy(app)
 
 
 class DecanterBaseModel(AbstractConcreteBase, db.Model, SerializationMixin):
+
     id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime)
-    modified_at = db.Column(db.DateTime)
+    created = db.Column(db.DateTime)
+    modified = db.Column(db.DateTime)
 
     def __init__(self):
         now = datetime.utcnow()
@@ -30,12 +31,23 @@ class DecanterBaseModel(AbstractConcreteBase, db.Model, SerializationMixin):
 
 roles_users = db.Table('roles_users',
                        DecanterBaseModel.metadata,
-                       db.Column('user_id',
-                                 db.Integer,
-                                 db.ForeignKey('user.id')),
-                       db.Column('role_id',
-                                 db.Integer,
-                                 db.ForeignKey('role.id')))
+                       db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+              )
+
+
+post_tag = db.Table('post_tag',
+                    DecanterBaseModel.metadata,
+                    db.Column('post_id', db.BigInteger, db.ForeignKey('post.id')),
+                    db.Column('tag_id', db.BigInteger, db.ForeignKey('tag.id'))
+           )
+
+
+comment_tag = db.Table('comment_tag',
+                       DecanterBaseModel.metadata,
+                       db.Column('comment_id', db.BigInteger, db.ForeignKey('comment.id')),
+                       db.Column('tag_id', db.BigInteger, db.ForeignKey('tag.id'))
+              )
 
 
 class Role(DecanterBaseModel, RoleMixin):
@@ -53,7 +65,7 @@ class Role(DecanterBaseModel, RoleMixin):
 class User(db.Model, UserMixin, SerializationMixin):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     username = db.Column(db.Unicode(255), nullable=False, unique=True, index=True)
     email = db.Column(db.Unicode(255), nullable=True, unique=True, index=True)
     password = db.Column(db.Unicode(32), nullable=False)
@@ -76,12 +88,57 @@ class User(db.Model, UserMixin, SerializationMixin):
         self.roles = kwargs['roles'] if 'roles' in kwargs else []
 
 
+# NOTE ::  It really feels like Post and Comment are two flavors of the same
+# NOTE ::  object type.  Perhaps they should be normalized together.
 class Post(DecanterBaseModel, RoleMixin):
     __tablename__ = 'post'
+
+    parent = db.Column(db.Integer, db.ForeignKey('comment.id'), nullable=True)
+
+    author = db.Column(db.BigInteger, db.ForeignKey('user.id'), index=True)
+    content = db.Column(db.Text, nullable=False)
+    slug = db.Column(db.Unicode(255), nullable=False, unique=True, index=True)
+    score = db.Column(db.BigInteger)
+    tags = db.relationship('Tag',
+                           secondary=post_tag,
+                           backref=db.backref('posts', lazy='dynamic')
+           )
 
 
 class Comment(DecanterBaseModel, RoleMixin):
     __tablename__ = 'comment'
+
     post = db.Column(db.Integer, db.ForeignKey('post.id'), index=True)
     parent = db.Column(db.Integer, db.ForeignKey('comment.id'), nullable=True)
 
+    author = db.Column(db.BigInteger, db.ForeignKey('user.id'), index=True)
+    content = db.Column(db.Text)
+    score = db.Column(db.BigInteger)
+    tags = db.relationship('Tag',
+                           secondary=comment_tag,
+                           backref=db.backref('comments', lazy='dynamic')
+           )
+
+
+class CommentRating(DecanterBaseModel, RoleMixin):
+    __tablename__ = 'comment_rating'
+
+    rater = db.Column(db.BigInteger, db.ForeignKey('user.id'), index=True)
+    rating = db.Column(db.Integer, nullable=False)
+    comment_id = db.Column(db.BigInteger, db.ForeignKey('comment.id'), index=True)
+    comment = db.relationship('Comment', backref='ratings')
+
+
+class PostRating(DecanterBaseModel, RoleMixin):
+    __tablename__ = 'post_rating'
+
+    rater = db.Column(db.BigInteger, db.ForeignKey('user.id'), index=True)
+    rating = db.Column(db.Integer, nullable=False)
+    post_id = db.Column(db.BigInteger, db.ForeignKey('post.id'), index=True)
+    post = db.relationship('Post', backref='ratings')
+
+
+class Tag(DecanterBaseModel, RoleMixin):
+    __tablename__ = 'tag'
+
+    slug = db.Column(db.Unicode(255), nullable=False, unique=True, index=True)
