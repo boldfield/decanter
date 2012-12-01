ad8e771265bf29a4ed9796f09fbe1931e54dbf89
Brian Oldfield <brian.oldfield@gmail.com>
2012-12-01 09:37:13 -0800

    Updating setup.py.
    Signed-off-by: Brian Oldfield <brian.oldfield@gmail.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+"""
+decanter
+========
+
+A blogging framework based on Python-Flask.  The project is in it's infancy,
+more details to come as things develop.
+
+"""
+import sys
+import os
 from setuptools import setup, find_packages
-import sys, os
 
 py = sys.version_info[:2]
 
-if py > (2, 7) or py < (2,5):
+if py > (2, 7) or py < (2, 5):
     raise RuntimeError('Python 2.5, 2.6 or 2.7 is required')
 
 root = os.path.dirname(os.path.abspath(__file__))
@@ -17,18 +26,26 @@ else:
 version = '0.0.1'
 
 install_requires = [
-    'Flask==0.9',
-    'Flask-SQLAlchemy==0.16',
-    'Flask-Testing==0.3',
-    'Flask-Security==1.2.3',
-    'SQLAlchemy==0.7.8',
-    'pytz==2012d',
+    'PyYAML==3.10',
+    'gunicorn==0.14.6',
+    'yuicompressor==2.4.7',
+    'itsdangerour==0.17',
+    'flask==0.9',
+    'flask-sqlalchemy==0.16',
+    'flask-script==0.3.3',
+    'flask-security==1.2.3',
+    'flask-admin==1.0.2',
+    'flask-assets',
+    'flask-seasurf',
+    'sqlalchemy==0.7.8',
+    'psycopg2==2.4.5',
 ]
 
 tests_require = [
     'nose==1.1.2',
     'mock==0.8.0',
     'coverage==3.5.1',
+    'flask-testing==0.3',
 ]
 
 entry_points = dict()
@@ -49,11 +66,10 @@ setup(name='decanter',
       install_requires=install_requires,
       entry_points=entry_points,
       classifiers=[
-        # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Operating System :: OS Independent',
-      ],
-)
+          # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Operating System :: OS Independent',
+      ])
