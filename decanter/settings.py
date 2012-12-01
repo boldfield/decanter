3d02d2e7112a797ad2d9250c739a4a4c5efa1014
Brian Oldfield <brian.oldfield@gmail.com>
2012-12-01 09:38:19 -0800

    Updating settings.
    Signed-off-by: Brian Oldfield <brian.oldfield@gmail.com>

--- decanter/settings.py
+++ decanter/settings.py
@@ -1,3 +1,7 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/decanter.test.db'
-DOMAIN = 'local.dev'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/decanter.test.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://decanter@localhost/decanter'
+DOMAIN = 'decanter.com'
+SECRET_KEY = 'development-key'
+
+SECURITY_USER_DATASTORE = 'user_datastore'
