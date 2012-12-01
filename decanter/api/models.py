d742b0a39c4b6cca59ea6bf6d1d54a5c984d011a
Brian Oldfield <brian.oldfield@gmail.com>
2012-12-01 09:26:41 -0800

    Adding api models.
    Signed-off-by: Brian Oldfield <brian.oldfield@gmail.com>

--- decanter/api/models.py
+++ decanter/api/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from hashlib import sha256 as sha
+from uuid import uuid4
 
 from sqlalchemy.ext.declarative import AbstractConcreteBase
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.security import UserMixin, RoleMixin
+from flask.ext.security.datastore.sqlalchemy import SQLAlchemyUserDatastore
 
 from decanter.mixins import SerializationMixin
 from decanter.api import create_app
@@ -17,7 +19,8 @@ class DecanterBaseModel(AbstractConcreteBase, db.Model, SerializationMixin):
     created = db.Column(db.DateTime)
     modified = db.Column(db.DateTime)
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super(DecanterBaseModel, self).__init__(*args, **kwargs)
         now = datetime.utcnow()
         self.created = now
         self.modified = now
@@ -29,11 +32,63 @@ class DecanterBaseModel(AbstractConcreteBase, db.Model, SerializationMixin):
         return self.__str__()
 
 
+# User/Group data modles
 roles_users = db.Table('roles_users',
-                       DecanterBaseModel.metadata,
-                       db.Column('user_id', db.BigInteger, db.ForeignKey('user.id')),
-                       db.Column('role_id', db.BigInteger, db.ForeignKey('role.id'))
-              )
+    db.Column('user_id', db.Integer(), db.ForeignKey('role.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('user.id')))
+
+
+class Role(DecanterBaseModel):
+    __tablename__ = 'role'
+
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+
+class User(DecanterBaseModel):
+    __tablename__ = 'user'
+
+    username = db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(120))
+    salt = db.Column(db.Unicode(32), nullable=False)  # This shouldn't be here 
+    active = db.Column(db.Boolean(), default=True)
+
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    def __init__(self, **kwargs):
+        if 'roles' not in kwargs:
+            kwargs['roles'] = []
+        else:
+            roles = list()
+            for role in kwargs['roles']:
+                r = Role.query.filter_by(name=role).first()
+                roles.append(r)
+            kwargs['roles'] = roles
+        super(User, self).__init__(**kwargs)
+        self.salt = uuid4().hex
+        self.password = self.encrypt(self.password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.username
+
+    def encrypt(self, password):
+        crypt_str = self._encrypt(self.salt, password)
+        return self._encrypt(self.salt, crypt_str)
+
+    def _encrypt(self, salt, message):
+        crypt_str = salt + message
+        return sha(crypt_str).hexdigest()
 
 
 post_tag = db.Table('post_tag',
@@ -50,54 +105,20 @@ comment_tag = db.Table('comment_tag',
               )
 
 
-class Role(DecanterBaseModel, RoleMixin):
-    __tablename__ = 'role'
-
-    name = db.Column(db.Unicode(80), unique=True)
-    description = db.Column(db.Unicode(255))
-
-    def __init__(self, *args, **kwargs):
-        super(Role, self).__init__()
-        self.name = unicode(kwargs['name'])
-        self.description = unicode(kwargs['description'])
-
-
-class User(db.Model, UserMixin, SerializationMixin):
-    __tablename__ = 'user'
-
-    id = db.Column(db.BigInteger, primary_key=True)
-    username = db.Column(db.Unicode(255), nullable=False, unique=True, index=True)
-    email = db.Column(db.Unicode(255), nullable=True, unique=True, index=True)
-    password = db.Column(db.Unicode(32), nullable=False)
-    active = db.Column(db.Boolean(), default=0)
-    confirmation_token = db.Column(db.Unicode(255))
-    confirmation_sent_at = db.Column(db.DateTime)
-    created_at = db.Column(db.DateTime)
-    modified_at = db.Column(db.DateTime)
-    roles = db.relationship('Role',
-                            secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'),
-            )
-
-    def __init__(self, *args, **kwargs):
-        super(User, self).__init__()
-        self.username = unicode(kwargs['username'])
-        self.email = unicode(kwargs['email'])
-        self.password = unicode(kwargs['password'])
-        self.active = kwargs['active'] if 'active' in kwargs else False
-        self.roles = kwargs['roles'] if 'roles' in kwargs else []
-
-
 # NOTE ::  It really feels like Post and Comment are two flavors of the same
 # NOTE ::  object type.  Perhaps they should be normalized together.
-class Post(DecanterBaseModel, RoleMixin):
+class Post(DecanterBaseModel):
     __tablename__ = 'post'
 
     parent = db.Column(db.BigInteger, db.ForeignKey('post.id'), nullable=True)
 
     author = db.Column(db.BigInteger, db.ForeignKey('user.id'), index=True)
+    active = db.Column(db.Boolean(), default=False)
+    title = db.Column(db.Unicode(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
     slug = db.Column(db.Unicode(255), nullable=False, unique=True, index=True)
+    published = db.Column(db.DateTime)
+
     score = db.Column(db.BigInteger)
     tags = db.relationship('Tag',
                            secondary=post_tag,
@@ -105,7 +126,7 @@ class Post(DecanterBaseModel, RoleMixin):
            )
 
 
-class Comment(DecanterBaseModel, RoleMixin):
+class Comment(DecanterBaseModel):
     __tablename__ = 'comment'
 
     post = db.Column(db.BigInteger, db.ForeignKey('post.id'), index=True)
@@ -120,7 +141,7 @@ class Comment(DecanterBaseModel, RoleMixin):
            )
 
 
-class CommentRating(DecanterBaseModel, RoleMixin):
+class CommentRating(DecanterBaseModel):
     __tablename__ = 'comment_rating'
 
     rater = db.Column(db.BigInteger, db.ForeignKey('user.id'), index=True)
@@ -129,7 +150,7 @@ class CommentRating(DecanterBaseModel, RoleMixin):
     comment = db.relationship('Comment', backref='ratings')
 
 
-class PostRating(DecanterBaseModel, RoleMixin):
+class PostRating(DecanterBaseModel):
     __tablename__ = 'post_rating'
 
     rater = db.Column(db.BigInteger, db.ForeignKey('user.id'), index=True)
@@ -138,7 +159,7 @@ class PostRating(DecanterBaseModel, RoleMixin):
     post = db.relationship('Post', backref='ratings')
 
 
-class Tag(DecanterBaseModel, RoleMixin):
+class Tag(DecanterBaseModel):
     __tablename__ = 'tag'
 
     slug = db.Column(db.Unicode(255), nullable=False, unique=True, index=True)
