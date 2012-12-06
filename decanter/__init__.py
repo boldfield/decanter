4e1051a773a5f7bccb181048f4952d9b8a48f565
Brian Oldfield <brian@oldfield.io>
2012-12-06 13:25:27 -0800

    Fixed Decanter.xhr_domain settings for staging environment. Requires DECANTER_STAGE_SUBDOMAIN envvar to be set.

--- decanter/__init__.py
+++ decanter/__init__.py
@@ -18,6 +18,7 @@ class Decanter(object, LoginManagerMixin):
     xhr_allow_origin = list()
     xhr_domains = os.environ.get('DECANTER_DOMAINS', '').split(',')
     xhr_schema = ('http', 'https')
+    stage_subdomain = os.environ.get('DECANTER_STAGE_SUBDOMAIN')
 
     def __init__(self,
                  default_app,
@@ -41,11 +42,11 @@ class Decanter(object, LoginManagerMixin):
 
         if admin_subdomain:
             self.subdomains[admin_subdomain] = create_web_app
-            self._add_xhr_origin_for_subdomain(admin_subdomain)
+            self._add_xhr_origin_for_subdomain(admin_subdomain, stage=self.stage_subdomain)
 
         if api_subdomain:
             self.subdomains[api_subdomain] = create_api_app
-            self._add_xhr_origin_for_subdomain(api_subdomain)
+            self._add_xhr_origin_for_subdomain(api_subdomain, stage=self.stage_subdomain)
 
         self.paths[admin_prefix or 'admin'] = create_web_app
         self.paths[api_prefix or 'api'] = create_api_app
@@ -72,16 +73,20 @@ class Decanter(object, LoginManagerMixin):
 
         return app(environ, start_response)
 
-    def _add_xhr_origin_for_subdomain(self, subdomain):
+    def _add_xhr_origin_for_subdomain(self, subdomain, stage=None):
         for domain in self.xhr_domains:
             for schema in self.xhr_schema:
-                origin = '%s://%s.%s' % (schema, subdomain, domain)
-                self.xhr_allow_origin.append(origin)
+                if stage is None:
+                    origin = '%s://%s.%s' % (schema, subdomain, domain)
+                    self.xhr_allow_origin.append(origin)
+                else:
+                    origin = '%s://%s.%s.%s' % (schema, stage, subdomain, domain)
+                    self.xhr_allow_origin.append(origin)
 
     def register_app(self, create_app, subdomain=None, path=None, strip_path=False):
         if subdomain:
             self.subdomains[subdomain] = create_app
-            self._add_xhr_origin_for_subdomain(subdomain)
+            self._add_xhr_origin_for_subdomain(subdomain, stage=self.stage_subdomain)
         if path:
             self.paths[path] = create_app
         if strip_path:
