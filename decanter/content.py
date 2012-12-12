43b583793b14b7a2d025a7f27e52f731821c0b27
Brian Oldfield <brian@oldfield.io>
2012-12-11 21:53:00 -0800

    Added new methods to decanter.content.S3Content
    delete and version_copy
    
    Signed-off-by: Brian Oldfield <brian@oldfield.io>

--- decanter/content.py
+++ decanter/content.py
@@ -20,20 +20,44 @@ class S3Content(object):
         self.bucket = Bucket(self.s3, name=self.bucket)
 
     def save(self, slug, version, format, content, domain):
-        name = self._content_name(slug, version, format)
-        key = os.path.join(domain,
-                           slug,
-                           name)
-        key = Key(bucket=self.bucket, name=key)
+        name = self._name(slug, version, format)
+        key = self._key(domain, slug, name)
 
         content_type = 'text/plain' if format == 'txt' else 'text/html'
         headers = {'Content-Type': content_type}
 
         key.set_contents_from_string(content, headers=headers)
         key.set_acl('public-read')
+        return key
+
+    def get(self, slug, version, format, domain):
+        name = self._name(slug, version, format)
+        key = self._key(domain, slug, name)
+        return key.get_contents_as_string()
+
+    def delete(self, slug, version, format, domain):
+        # TODO :: Error handling needs to be more refined
+        try:
+            name = self._name(slug, version, format)
+            key = self._key(domain, slug, name)
+            key.delete()
+        except:
+            return False
+        return True
+
+    def version_copy(self, slug, format, domain, from_version, to_version, readable=False):
+        # TODO :: Error handling needs to be more refined
+        try:
+            content = self.get(slug, from_version, format, domain)
+            key = self.save(slug, to_version, format, content, domain)
+        except:
+            return False
+        if readable:
+            key.set_acl('public-read')
+        return True
 
     def url(self, slug, version, format, domain):
-        name = self._content_name(slug, version, format)
+        name = self._name(slug, version, format)
         url = os.path.join(self.s3_url,
                            domain,
                            slug,
@@ -44,5 +68,11 @@ class S3Content(object):
     def close(self, *args, **kwargs):
         self.s3.close()
 
-    def _content_name(self, slug, version, format):
-        return '%s-v%s.%s' % (slug, version, format)
+    def _name(self, slug, version, format):
+        return '%s-%s.%s' % (slug, version, format)
+
+    def _key(self, domain, slug, name):
+        key = os.path.join(domain,
+                           slug,
+                           name)
+        return Key(bucket=self.bucket, name=key)
